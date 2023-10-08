8193faa2c6511ea04f06ce9739f4d48a04b738cf
Atharva Rekhawar <atharvarekhawar64@gmail.com>
2023-10-09 00:06:00 +0530

    initial navbar and homepage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
-
+import Home from "./components/Home"
+import About from "./components/About"
+import Services from "./components/Services"
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
 function App() {
-
   return (
-    <>
-      <div className="text-red-800 text-5xl">
-        HEllo World
-      </div>
-    </>
+    <div className="overflow-x-hidden">
+      <Home/>
+      <About/>
+      <Services/>
+      <Projects/>
+      <Contact/>
+    </div>
   )
 }
 
