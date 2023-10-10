e76d3baefd8e320e800cccfdb9b3936f75594464
Atharva Rekhawar <atharvarekhawar64@gmail.com>
2023-10-10 19:09:49 +0530

    Logo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import "./Navbar.css";
 import NavDropdown from "./NavDropdown";
 import { Link } from "react-scroll";
+import Logo from "../assets/images/LOGO.png";
+
 const Navbar = () => {
   return (
-    <div className="absolute flex justify-between items-center top-[4%] left-[8%] right-[8%] p-[10px] border-b-[1px] border-[#333232]">
-      <div className="flex gap-2">
+    <div className="absolute flex justify-between items-center top-[3%] left-[8%] right-[8%] p-[10px] border-b-[1px] border-[#333232]">
+      <div className="flex items-center gap-2">
         <div className="md:hidden">
           <NavDropdown />
         </div>
-        <div>LOGO</div>
+        <img className="rounded-full cursor-pointer" src={Logo} alt="Logo" width={70} />
       </div>
       <div className="hidden md:flex text-white gap-4  ">
         <div className="btn-pink">
